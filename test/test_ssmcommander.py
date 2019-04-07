4fd440241d04e4445b10a9e368bb94720a490e9d
DJCrabhat <djcrabhat@sosimplerecords.com>
2019-04-07 14:59:57 -0700

    skeleton travis

---
+++ test/test_ssmcommander.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        # TODO: real tests
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
