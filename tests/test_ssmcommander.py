7572156c75a56289a15c0e2d54467e62eae4b25d
DJCrabhat <djcrabhat@sosimplerecords.com>
2019-05-28 10:55:26 -0700

    Add KMS decryption

--- tests/test_ssmcommander.py
+++ tests/test_ssmcommander.py
@@ -1,10 +1,17 @@
 import unittest
-
+from ssmcommander.yaml_parser import InputFile
+import click.exceptions
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_bad_kms_block(self):
         # TODO: real tests
-        self.assertEqual(True, True)
+        with self.assertRaises(click.exceptions.BadParameter):
+            file = InputFile("test_badkms.yml")
+
+    def test_can_read(self):
+        file = InputFile("test.yml")
+        self.assertIsNotNone(file)
+
 
 
 if __name__ == '__main__':
