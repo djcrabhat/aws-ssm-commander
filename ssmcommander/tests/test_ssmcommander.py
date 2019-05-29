cb73f87fb56a9b35c41af05a953b94d5643df75b
DJCrabhat <djcrabhat@sosimplerecords.com>
2019-05-28 21:32:41 -0700

    add kms stuff to readme, enhance test

--- ssmcommander/tests/test_ssmcommander.py
+++ ssmcommander/tests/test_ssmcommander.py
@@ -1,10 +1,8 @@
 import unittest
 from ssmcommander.yaml_parser import InputFile
-import click.exceptions
 import os
 
 from moto import mock_kms
-
 
 
 class MyTestCase(unittest.TestCase):
@@ -13,6 +11,8 @@ class MyTestCase(unittest.TestCase):
         # mocked kms decrypt is just double base64'ed
         input = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_badkms.yml")
         file = InputFile(input)
+        self.assertIsNotNone(input)
+        self.assertEqual(file.raw_data['section_bad_kms_value']['password'], "boblawblob")
 
     def test_can_read(self):
         input = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test.yml")
