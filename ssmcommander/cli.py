6f37b9d850988981ed8f469f2432824dec3cfa33
DJCrabhat <djcrabhat@sosimplerecords.com>
2020-03-06 19:47:39 -0800

    add --ssm-override-url and --kms-override-url params

--- ssmcommander/cli.py
+++ ssmcommander/cli.py
@@ -1,18 +1,21 @@
 import click
 from ssmcommander.tree import Tree
 from ssmcommander.yaml_parser import InputFile
+from ssmcommander import CommanderSession
 import logging
 import boto3
 
 log = logging.getLogger()
 
 
-def get_session(region=None):
-    if region:
-        session = boto3.Session(region_name=region)
-    else:
-        session = boto3.Session()
-    return session
+def get_session(region=None, ssm_override_url=None, kms_override_url=None):
+    overrides = {}
+    if ssm_override_url:
+        overrides["ssm"] = ssm_override_url
+    if kms_override_url:
+        overrides["kms"] = kms_override_url
+
+    return CommanderSession(region_name=region, endpoint_overrides=overrides)
 
 
 @click.group()
@@ -33,21 +36,23 @@ def cli(debug):
 
 @click.command()
 @click.option('--region', prompt=False)
+@click.option('--ssm-override-url', prompt=False)
 @click.argument('ssm_prefix')
-def dump(region, ssm_prefix):
+def dump(region, ssm_prefix, ssm_override_url=None):
     """Dump the values in ssm to a param file, for backup or inspection.  Suitable for sending in to the write command"""
-    session = get_session(region)
+    session = get_session(region, ssm_override_url=ssm_override_url)
     tree = Tree(ssm_prefix, session)
     tree.fetch_from_ssm(with_decryption=True)
     tree.dump()
 
 
-@click.command()
+@click.command("tree")
+@click.option('--ssm-override-url', prompt=False)
 @click.option('--region', prompt=False)
 @click.argument('ssm_prefix')
-def tree(region, ssm_prefix):
+def tree_func(region, ssm_prefix, ssm_override_url=None):
     """Print out a tree of your SSM parameters"""
-    session = get_session(region)
+    session = get_session(region, ssm_override_url=ssm_override_url)
     tree = Tree(ssm_prefix, session)
     tree.fetch_from_ssm()
     tree.echo()
@@ -55,23 +60,20 @@ def tree(region, ssm_prefix):
 
 @click.command()
 @click.option('--region', prompt=False)
+@click.option('--ssm-override-url', prompt=False)
+@click.option('--kms-override-url', prompt=False)
 @click.option('--overwrite/--no-overwrite', default=False, prompt=False)
 @click.argument('ssm_prefix')
 @click.argument('file')
-def write(region, ssm_prefix, file, overwrite):
+def write(region, ssm_prefix, file, overwrite, ssm_override_url=None, kms_override_url=None):
     """take a yaml file, put it in to SSM"""
     parsed_file = InputFile(file)
-
-    if region:
-        session = boto3.Session(region_name=region)
-    else:
-        session = boto3.Session()
-
+    session = get_session(region, ssm_override_url=ssm_override_url, kms_override_url=kms_override_url)
     parsed_file.write_to_ssm(session, ssm_prefix, overwrite=overwrite)
 
 
 cli.add_command(dump)
-cli.add_command(tree)
+cli.add_command(tree_func)
 cli.add_command(write)
 
 if __name__ == "__main__":
